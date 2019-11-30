d39adca7ebffcdd5cd450792a9dafa53a1945ac8
Kinga Almási <kinga.almasi@stud.uni-corvinus.hu>
2019-11-30 10:50:30 +0100

    x2

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ namespace UserMaintenance
             InitializeComponent();
             lblLastName.Text = Resource.FullName;
             btnAdd.Text = Resource.Add;
+            btnToF.Text = Resource.ToFile;
 
             listUsers.DataSource = users;
             listUsers.ValueMember = "ID";
@@ -32,6 +34,25 @@ namespace UserMaintenance
                 FullName = txtLastName.Text,
             };
             users.Add(u);
+        }
+
+        private void btnToF_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog(); 
+            sfd.AddExtension = true; 
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                
+                foreach (var u in users)
+                {
+                    sw.Write(u.ID);
+                    sw.Write(": ");
+                    sw.Write(u.FullName);
+                    sw.WriteLine();
+                }
+            }
         }
     }
 }
